e19535f9f03dfb2993b15178e05e63615fb300ed
pauldekarin <bluerayy123@gmail.com>
2024-03-29 23:06:39 +0700

    rocket

--- assets/scripts/Rocket.cs
+++ assets/scripts/Rocket.cs
@@ -1,12 +1,20 @@
 using Godot;
 using System;
 
-public partial class Rocket : CsgCylinder3D
+public partial class Rocket : CsgCombiner3D
 {
+	private Vector3 speed = new Vector3(.1f,.1f,.1f);
+	private float ratio = .5f;
 	public override void _Ready()
 	{
 		base._Ready();
-		Console.Write("Help");
-		Position = new Vector3(1,1,1);
+		
+	}
+	public override void _PhysicsProcess(double delta)
+	{
+		base._PhysicsProcess(delta);
+		CharacterBody3D target = (CharacterBody3D)GetNode("/root").GetChild(0).GetNode("Player");
+		Vector3 target_pos = target.Position;
+		Rotation = Rotation.AngleTo(target_pos);
 	}
 }

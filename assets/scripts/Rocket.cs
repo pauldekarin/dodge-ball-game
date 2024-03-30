0733901e9ceefdce772c7ebb6d6a5639706382f8
pauldekarin <bluerayy123@gmail.com>
2024-03-30 16:06:42 +0700

    rocket

--- assets/scripts/Rocket.cs
+++ assets/scripts/Rocket.cs
@@ -2,13 +2,13 @@ using Godot;
 using System;
 
 
-public partial class Rocket : CsgCombiner3D
+public partial class Rocket : StaticBody3D
 {
-	
+	CharacterBody3D Target;
 	Vector3 axis = Vector3.Left;
-	private float speed = .3f;
-	private Vector3 f = new Vector3(0,.7f,.7f)*5;
-	private float ratio = .1f;
+	private float Speed = .3f;
+	public Vector3 Force = new Vector3(0,.7f,.7f)*5;
+	private float Damp = .1f;
 	public static Vector3 Lerp(Vector3 First, Vector3 Second, float amount){
 		float x = Mathf.Lerp(First.X, Second.X, amount);
 		float y = Mathf.Lerp(First.Y, Second.Y, amount);
@@ -20,23 +20,36 @@ public partial class Rocket : CsgCombiner3D
 	{
 		
 		base._Ready();
+		_Spawn();
+		Target = (CharacterBody3D)GetTree().Root.GetNode("Scene").GetNode("Player");
 		
-		
-		
+	}
+	public void _Spawn(){
+		GD.Randomize();
+
+		Position = ((new Vector3(
+			GD.Randf(),
+			GD.Randf(),
+			GD.Randf()
+		))  - Vector3.One/2) * 25;
+		Force = (new Vector3(
+			GD.Randf(),
+			GD.Randf(),
+			GD.Randf()
+		)) - Vector3.One / 2;
 	}
 	public override void _PhysicsProcess(double delta)
 	{
-		
 		base._PhysicsProcess(delta);
-		
-		CharacterBody3D Target = (CharacterBody3D)GetNode("/root").GetChild(0).GetNode("Player");
-		Vector3 DirectionVector = (Target.GlobalTransform.Origin - Position).Normalized()*speed + f;
-		Position += DirectionVector.Normalized()*speed;
-		LookAt(Position + DirectionVector, Vector3.Left);
-
-
-		f = Lerp(f, Vector3.Zero, .1f);
-		// GD.Print(f);
+		Vector3 Direction = Position.DirectionTo(Target.Position);
+		Vector3 DirectionVector = (Target.GlobalTransform.Origin - Position).Normalized()*Speed + Force;
+		KinematicCollision3D Collision =  MoveAndCollide(DirectionVector.Normalized()*Speed);
+		if(Collision != null && Collision.GetCollider().Equals(Target)){
+			_Spawn();
+		}else{
+			LookAt(DirectionVector + Position);
+			Force = Lerp(Force, Vector3.Zero, Damp);
+		}
 	}
 }
 

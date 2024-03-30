948801b357ddeae6e75294289db998457d0d3b55
pauldekarin <bluerayy123@gmail.com>
2024-03-30 13:57:42 +0700

    rocket

--- assets/scripts/Rocket.cs
+++ assets/scripts/Rocket.cs
@@ -6,27 +6,37 @@ public partial class Rocket : CsgCombiner3D
 {
 	
 	Vector3 axis = Vector3.Left;
-	private Vector3 speed = new Vector3(0,.1f,0);
-	private float ratio = .01f;
+	private float speed = .3f;
+	private Vector3 f = new Vector3(0,.7f,.7f)*5;
+	private float ratio = .1f;
+	public static Vector3 Lerp(Vector3 First, Vector3 Second, float amount){
+		float x = Mathf.Lerp(First.X, Second.X, amount);
+		float y = Mathf.Lerp(First.Y, Second.Y, amount);
+		float z = Mathf.Lerp(First.Z, Second.Z, amount);
+
+		return new Vector3(x,y,z);
+	}
 	public override void _Ready()
 	{
 		
 		base._Ready();
+		
+		
 		
 	}
 	public override void _PhysicsProcess(double delta)
 	{
 		
 		base._PhysicsProcess(delta);
-		CharacterBody3D target = (CharacterBody3D)GetNode("/root").GetChild(0).GetNode("Player");
-		Vector3 target_pos = target.Position;
-		Vector3 direction = (target_pos - Position).Normalized();
-		float x = Mathf.Lerp(speed.Normalized().X, direction.X, 1f);
-		float y = Mathf.Lerp(speed.Normalized().Y, direction.Y, 1f);
-		float z = Mathf.Lerp(speed.Normalized().Z, direction.Z, 1f);
-		Vector3 step = new Vector3(x,y,z);
-		step *= ratio;
-		Position += step;
 		
+		CharacterBody3D Target = (CharacterBody3D)GetNode("/root").GetChild(0).GetNode("Player");
+		Vector3 DirectionVector = (Target.GlobalTransform.Origin - Position).Normalized()*speed + f;
+		Position += DirectionVector.Normalized()*speed;
+		LookAt(Position + DirectionVector, Vector3.Left);
+
+
+		f = Lerp(f, Vector3.Zero, .1f);
+		// GD.Print(f);
 	}
 }
+
